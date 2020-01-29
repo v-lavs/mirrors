12986e47df4af9828032f1e2a3e5cbd0d7e34d47
v-lavs <lavskavanda@gmail.com>
2020-01-29 12:35:36 +0200

    added structure page home, section-banner, section-product

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+/**
+ *To include js file from libraries write: `//= include ./path-to-file`
+ * */
+
+//= include ../lib/jquery-3.3.1.min.js
+//= include ../lib/owl-slider/js/build.js
+
+
+/**
+ * CUSTOM SCRIPTS
+ **/
+
+$(document).ready(function () {
+
+    var nav = $('.main-nav');
+
+    /**
+     * MOB MENU SCRIPT
+     **/
+
+    $('.burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.close-nav, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+    /**
+     * OWL-CAROUSEL SCRIPT
+     **/
+    jQuery("#slider-carousel").owlCarousel({
+        items: 3,
+        pagination: false,
+        dots: false,
+        nav: true,
+        slideSpeed: 2000,
+        margin: 0,
+
+    });
+
+});
